375e21734c3e2e836a1b86d8fcab6d82b1127923
horton <hthlai@students.cs.ubc.ca>
2022-10-02 21:36:23 -0700

    Finished implementation of Wallet

--- src/main/model/Wallet.java
+++ src/main/model/Wallet.java
@@ -1,34 +1,45 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Represents a wallet
 public class Wallet {
+    private List<CreditCard> creditCardList;
 
     // EFFECTS: creates a new wallet object
     public Wallet() {
-        // stub
+        creditCardList = new ArrayList<>();
     }
 
     // REQUIRES: newCard is not already in wallet
     // MODIFIES: this
     // EFFECTS: adds newCard to wallet
     public void addCreditCard(CreditCard newCard) {
-        // stub
+        creditCardList.add(newCard);
     }
 
     // EFFECTS: returns list of names of credit cards in wallet
     public List<String> viewCreditCardList() {
-        return null; // stub
+        List<String> cardNameList = new ArrayList<>();
+        for (CreditCard c: creditCardList) {
+            cardNameList.add(c.getName());
+        }
+        return cardNameList;
     }
 
     // EFFECTS: returns credit card with given name
     public CreditCard selectCreditCard(String name) {
-        return null; // stub
+        for (CreditCard c: creditCardList) {
+            if (c.getName().equals(name)) {
+                return c;
+            }
+        }
+        return null;
     }
 
     // EFFECTS: returns list of credit cards in wallet
     public List<CreditCard> getCreditCardList() {
-        return null; // stub
+        return creditCardList;
     }
 }
