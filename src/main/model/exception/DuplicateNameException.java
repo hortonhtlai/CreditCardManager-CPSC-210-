dfb500c7374f4f68bbd633629ab367f0cf1229a2
horton <hthlai@students.cs.ubc.ca>
2022-11-01 21:49:36 -0700

    Added constructors for exception classes

--- src/main/model/exception/DuplicateNameException.java
+++ src/main/model/exception/DuplicateNameException.java
@@ -2,4 +2,6 @@ package model.exception;
 
 // Represents the exception that occurs when the user enters a non-unique name as the name of a new credit card
 public class DuplicateNameException extends Exception {
+    public DuplicateNameException() {
+    }
 }
--- src/main/model/exception/EmptyNameException.java
+++ src/main/model/exception/EmptyNameException.java
@@ -2,4 +2,6 @@ package model.exception;
 
 // Represents the exception that occurs when the user enters an empty name as the name of a new credit card
 public class EmptyNameException extends Exception {
+    public EmptyNameException() {
+    }
 }
